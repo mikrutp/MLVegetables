0ba077ade4f5409063dbacdc5729bce78f78d3b7
mikrutp <piotrmikrut98@gmail.com>
2022-01-27 23:15:46 +0100

    final project

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import matplotlib.pyplot as plt
 import numpy as np
-import os
-import PIL
 import tensorflow as tf
 
 from tensorflow import keras
@@ -11,17 +8,18 @@ import pathlib
 
 if __name__ == '__main__':
 
-
     data_dir = pathlib.Path("D://AAinfa/sem2/AnObCyfr/trainset")
+    print("\ndata_dir :\n")
     print(data_dir)
 
     image_count = len(list(data_dir.glob('*/*.jpg')))
+    print("\nimage_count :\n")
     print(image_count)
 
     batch_size = 32
     img_height = 100
     img_width = 100
-
+# podzielenie obrazkow na zbiór treningowy oraz zbiór validacji 80 do 20
     train_ds = tf.keras.utils.image_dataset_from_directory(
         data_dir,
         validation_split=0.2,
@@ -39,10 +37,12 @@ if __name__ == '__main__':
         batch_size=batch_size)
 
     class_names = train_ds.class_names
+    print("\nDataset :\n")
     print(class_names)
 
     import matplotlib.pyplot as plt
 
+# Pierwsze 9 obrazkow z bazy treningowej
     plt.figure(figsize=(10, 10))
     for images, labels in train_ds.take(1):
         for i in range(9):
@@ -51,15 +51,24 @@ if __name__ == '__main__':
             plt.title(class_names[labels[i]])
             plt.axis("off")
 
+    # W razie potrzeby mozna iterowac recznie
     for image_batch, labels_batch in train_ds:
+        print("\nThis is a batch of 32 images of shape 100x100x3 (the last dimension refers to color channels RGB) :\n")
         print(image_batch.shape)
         print(labels_batch.shape)
         break
+    # Image Batch jest to tensor o kształcie (32, 100, 100, 3). Na ten batch składa się 32 obrazki
 
+    # Automatyczna konfiguracja dataset w celach wydajnosciowych
     AUTOTUNE = tf.data.AUTOTUNE
 
     train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
     val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
+
+
+    # Standaryzowanie danych
+    # Wartości RGB w zakresie [0,255] nie są najlepsze dla sieci nauronowych,
+    # dlatego standaryzujemy wartosci do zakresu [0, 1]
 
     normalization_layer = layers.Rescaling(1. / 255)
 
@@ -67,10 +76,13 @@ if __name__ == '__main__':
     image_batch, labels_batch = next(iter(normalized_ds))
     first_image = image_batch[0]
     # Notice the pixel values are now in `[0,1]`.
+    print("\n pixele sa od 0.0 do 1.0 \n")
     print(np.min(first_image), np.max(first_image))
 
-    num_classes = len(class_names)
 
+    # Tworzenie modelu
+
+    num_classes = len(class_names)
     model = Sequential([
         layers.Rescaling(1. / 255, input_shape=(img_height, img_width, 3)),
         layers.Conv2D(16, 3, padding='same', activation='relu'),
@@ -84,18 +96,25 @@ if __name__ == '__main__':
         layers.Dense(num_classes)
     ])
 
+
+    # Komplitacja modelu
+    # Optymalizacja Adama to stochastyczna metoda opadania gradientu
+    # oparta na adaptacyjnej estymacji momentów pierwszego i drugiego rzędu.
+
     model.compile(optimizer='adam',
                   loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
                   metrics=['accuracy'])
-
     model.summary()
 
-    epochs = 1
+    # Trenowanie modelu
+    epochs = 5
     history = model.fit(
         train_ds,
         validation_data=val_ds,
         epochs=epochs
     )
+
+    # Wizualizacja wynikow treningu - dwa wykresy
 
     acc = history.history['accuracy']
     val_acc = history.history['val_accuracy']
@@ -119,79 +138,8 @@ if __name__ == '__main__':
     plt.title('Training and Validation Loss')
     plt.show()
 
-    data_augmentation = keras.Sequential(
-        [
-            layers.RandomFlip("horizontal",
-                              input_shape=(img_height,
-                                           img_width,
-                                           3)),
-            layers.RandomRotation(0.1),
-            layers.RandomZoom(0.1),
-        ]
-    )
-
-    plt.figure(figsize=(10, 10))
-    for images, _ in train_ds.take(1):
-        for i in range(9):
-            augmented_images = data_augmentation(images)
-            ax = plt.subplot(3, 3, i + 1)
-            plt.imshow(augmented_images[0].numpy().astype("uint8"))
-            plt.axis("off")
-
-    model = Sequential([
-        data_augmentation,
-        layers.Rescaling(1. / 255),
-        layers.Conv2D(16, 3, padding='same', activation='relu'),
-        layers.MaxPooling2D(),
-        layers.Conv2D(32, 3, padding='same', activation='relu'),
-        layers.MaxPooling2D(),
-        layers.Conv2D(64, 3, padding='same', activation='relu'),
-        layers.MaxPooling2D(),
-        layers.Dropout(0.2),
-        layers.Flatten(),
-        layers.Dense(128, activation='relu'),
-        layers.Dense(num_classes)
-    ])
-
-    model.compile(optimizer='adam',
-                  loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-                  metrics=['accuracy'])
-
-    model.summary()
-
-    epochs = 2
-    history = model.fit(
-        train_ds,
-        validation_data=val_ds,
-        epochs=epochs
-    )
-
-
-    acc = history.history['accuracy']
-    val_acc = history.history['val_accuracy']
-
-    loss = history.history['loss']
-    val_loss = history.history['val_loss']
-
-    epochs_range = range(epochs)
-
-    plt.figure(figsize=(8, 8))
-    plt.subplot(1, 2, 1)
-    plt.plot(epochs_range, acc, label='Training Accuracy')
-    plt.plot(epochs_range, val_acc, label='Validation Accuracy')
-    plt.legend(loc='lower right')
-    plt.title('Training and Validation Accuracy')
-
-    plt.subplot(1, 2, 2)
-    plt.plot(epochs_range, loss, label='Training Loss')
-    plt.plot(epochs_range, val_loss, label='Validation Loss')
-    plt.legend(loc='upper right')
-    plt.title('Training and Validation Loss')
-    plt.show()
-
-    # sunflower_url = "https://storage.googleapis.com/download.tensorflow.org/example_images/592px-Red_sunflower.jpg"
-    # sunflower_path = tf.keras.utils.get_file('Red_sunflower', origin=sunflower_url)
-    corn_path = ("D://AAinfa/sem2/AnObCyfr/trainset/Corn/2_100.jpg")
+    # testowanie nowymi danymi
+    corn_path = ("D://AAinfa/sem2/AnObCyfr/trainset/testowyCorn.jpg")
 
     img = tf.keras.utils.load_img(
         corn_path, target_size=(img_height, img_width)
